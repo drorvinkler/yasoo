9b76703801073961ce2770bffdfaabccaeb6e6be
Dror <drorvinkler@gmail.com>
2020-05-08 14:48:21 +0300

    Version 0.4.0

---
+++ CHANGELOG.rst
@@ -0,0 +1,7 @@
+Changelog
+=========
+
+0.4.0 (2020-05-08)
+-------------------
+- Supporting preservation of iterable types even when `type_key` is `None`, though type hints.
+- Changed all warnings to use `warnings` instead of `logging`.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yasoo",
-    version="0.3.0",
+    version="0.4.0",
     author="Dror A. Vinkler",
     description="Yet another serializer of objects",
     long_description=long_description,
