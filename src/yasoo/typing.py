520a8c4c3a00d8e2ff5815fc7f5e3038d12b7c83
Dror <drorvinkler@gmail.com>
2020-09-06 12:46:57 +0300

    Fix to support python 3.6

--- src/yasoo/typing.py
+++ src/yasoo/typing.py
@@ -4,44 +4,47 @@ K = TypeVar("K")
 V = TypeVar("V")
 
 
-class List_:
+class _List:
     @overload
-    def __class_getitem__(cls, item: Type[V]) -> Callable[[], List[V]]:
+    def __getitem__(self, item: Type[V]) -> Callable[[], List[V]]:
         ...
 
-    def __class_getitem__(cls, item: Callable[[], V]) -> Callable[[], List[V]]:
+    def __getitem__(self, item: Callable[[], V]) -> Callable[[], List[V]]:
         return List[item]
 
 
-class Set_:
+class _Set:
     @overload
-    def __class_getitem__(cls, item: Type[V]) -> Callable[[], Set[V]]:
+    def __getitem__(self, item: Type[V]) -> Callable[[], Set[V]]:
         ...
 
-    def __class_getitem__(cls, item: Callable[[], V]) -> Callable[[], Set[V]]:
+    def __getitem__(self, item: Callable[[], V]) -> Callable[[], Set[V]]:
         return Set[item]
 
 
-class Dict_:
+class _Dict:
     @overload
-    def __class_getitem__(
-        cls, item: Tuple[Type[K], Type[V]]
+    def __getitem__(self, item: Tuple[Type[K], Type[V]]) -> Callable[[], Dict[K, V]]:
+        ...
+
+    @overload
+    def __getitem__(
+        self, item: Tuple[Type[K], Callable[[], V]]
     ) -> Callable[[], Dict[K, V]]:
         ...
 
     @overload
-    def __class_getitem__(
-        cls, item: Tuple[Type[K], Callable[[], V]]
+    def __getitem__(
+        self, item: Tuple[Callable[[], K], Type[V]]
     ) -> Callable[[], Dict[K, V]]:
         ...
 
-    @overload
-    def __class_getitem__(
-        cls, item: Tuple[Callable[[], K], Type[V]]
-    ) -> Callable[[], Dict[K, V]]:
-        ...
-
-    def __class_getitem__(
-        cls, item: Tuple[Callable[[], K], Callable[[], V]]
+    def __getitem__(
+        self, item: Tuple[Callable[[], K], Callable[[], V]]
     ) -> Callable[[], Dict[K, V]]:
         return Dict[item[0], item[1]]
+
+
+List_ = _List()
+Set_ = _Set()
+Dict_ = _Dict()
